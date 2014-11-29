64e4bce5056a965530ce9b2aae550d394c9dddbe
Andrew Seeley <awseeley@gmail.com>
2014-11-29 19:40:43 +1100

    Initial Commit

---
+++ SeemuSwiftiAd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeemuSwiftiAd
+//
+//  Created by Andrew Seeley on 29/11/2014.
+//  Copyright (c) 2014 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
